26594bd8b59126ba0b7a3eec91a6a36ebdadf14b
Julian JT <julian.tabaku@outlook.com>
2020-02-17 17:12:06 +0100

    small change on the organigram view model

--- HRMS.Web/Models/OrganigramViewModel.cs
+++ HRMS.Web/Models/OrganigramViewModel.cs
@@ -9,7 +9,7 @@ namespace HRMS.Web.Models
         public string Name { get; set; }
         public Guid Id { get; set; }
         public Guid CompanySiteId { get;  set; }
-        public bool IsCeo { get; internal set; }
+        public bool IsCeo { get;  set; }
         public Guid RespondsToId { get;  set; }
         public string CompanySite { get;  set; }
         public string RespondsTo { get;  set; }
